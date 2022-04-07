0e9b9f42bbc4a04e2c7a6a59d8d70137630a2901
merendamattia <46557019+merendamattia@users.noreply.github.com>
2022-04-07 12:36:02 +0200

    Upload ReverseString

---
+++ java/EserciziElly/src/ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static String reverse(String s){
+        String new_string = "";
+
+        for(int i = s.length() - 1; i >= 0; i--)
+           new_string += s.charAt(i);
+
+        return new_string;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Inserire una stringa: ");
+        String s = sc.nextLine();
+
+        System.out.println("Reverse: " + reverse(s));
+    }
+}
