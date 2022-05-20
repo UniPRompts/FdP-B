73aff6c7bb50c51d2f43eb9632b5ba60ce0aacfb
merendamattia <46557019+merendamattia@users.noreply.github.com>
2022-05-20 13:04:45 +0200

    Modifica tipo. Adesso accetta qualsiasi tipo generico, prima solo tipi Articolo

--- java/EserciziElly/src/catalogo/Catalogo.java
+++ java/EserciziElly/src/catalogo/Catalogo.java
@@ -1,36 +1,57 @@
 package catalogo;
 
+/*
+    Si implementi un'interfaccia Articolo che rappresenta articoli commerciali,
+    dotata dei metodi getNome() che restituisce il nome dell'articolo,
+    getPrezzo() che restituisce il prezzo dell'articolo e String toString() che
+    restituisce una descrizione (nome e prezzo) dell'articolo.
+    Si scriva una classe Catalogo<E> che rappresenta un catalogo di articoli commerciali
+    (limitare opportunamente il tipo generico E). La classe memorizza gli articoli in un
+    insieme ed è dotata dei metodi aggiungiArticolo(E articolo) che aggiunge un articolo
+    al catalogo, Set<E> getArticoliSottoPrezzo(double prezzo) che restituisce un insieme
+    di articoli il cui prezzo è minore del valore passato come parametro, e toString()
+    che stampa l'intero catalogo. Implementare inoltre i metodi hashCode e equals
+    della classe Catalogo<E>.
+*/
+
 import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
-public class Catalogo {
-    public HashSet<Articolo> set;
+public class Catalogo<T extends Articolo> {
+    public HashSet<T> set;
     private String nome;
 
-    public String getNome(){
-        return this.nome;
-    }
-
+    // Costruttore
     public Catalogo(String nome){
         this.nome = nome;
         set = new HashSet<>();
     }
 
-    public void addArticolo(Articolo other){
-        set.add(other);
+    // Getter
+    public String getNome(){
+        return this.nome;
     }
-
     public int numeroArticoli(){
         return set.size();
     }
 
-    public HashSet<Articolo> getArticoliSottoPrezzo(Double prezzo){
-        HashSet<Articolo> new_set = new HashSet<>();
+    // Setter
+    public void addArticolo(T other){
+        set.add(other);
+    }
+
+
+    /**
+     * Restituisce un insieme di articoli il cui prezzo è minore del valore passato come parametro
+     * @param prezzo
+     * @return
+     */
+    public HashSet<T> getArticoliSottoPrezzo(Double prezzo){
+        HashSet<T> new_set = new HashSet<>();
 
         for(Articolo i : set)
             if(i.getPrezzo() < prezzo)
-                new_set.add(i);
+                new_set.add((T) i);
 
         return new_set;
     }
@@ -39,7 +60,7 @@ public class Catalogo {
     public String toString(){
         String str = "Nome catalogo: " + nome;
 
-        for(Articolo i : set)
+        for(T i : set)
             str += "\n" + i.toString();
 
         return str;
